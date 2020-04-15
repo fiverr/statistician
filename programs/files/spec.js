acf0cdaadbfb56e5ff57892313cf1a32b1f481c3
Michael Schmidt <michael@fiverr.com>
2020-04-15 14:25:02 -0500

    Remove file hashes for comparison purposes (#7)

--- programs/files/spec.js
+++ programs/files/spec.js
@@ -8,6 +8,7 @@ describe('files', function() {
 		const result = await execute('./cli.js files --dir "./fixtures/files"');
 		expect(() => JSON.parse(result)).not.to.throw();
 	});
+
 	it('Should exclude the directory path from file sizes', async () => {
 		const result = await execute('./cli.js files --dir "./fixtures/files"');
 
@@ -15,6 +16,16 @@ describe('files', function() {
 			expect(filename).to.not.include('fixtures/files');
 		})
 	});
+
+    it('Should exclude fingerprints from file names in file summary', async () => {
+        const result = await execute('./cli.js files --dir "./fixtures/files/hashed_files"');
+
+        Object.keys(JSON.parse(result)).forEach(filename => {
+            expect(filename).to.not.include('998a22f8aa629b9f5441');
+            expect(filename).to.not.include('57c5a8e01c4023125eae');
+            expect(filename).to.match(/index.(js|css)$/);
+        })
+    });
 
 	it('Should exclude the directory path from file sizes', async () => {
 		{
